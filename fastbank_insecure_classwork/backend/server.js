2abf5e63e42ec456315b02a120240c06b469a482
cs6897 <chloe.n.stone@gmail.com>
2025-12-09 23:02:39 -0500

    Update server.js

--- fastbank_insecure_classwork/backend/server.js
+++ fastbank_insecure_classwork/backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.disable("x-powered-by");
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'strict-dynamic'; script-src 'self'; style-src 'self'; img-src 'self'; connect-src 'self'; form-action 'self'; frame-ancestors 'none'; base-uri 'self'");
+  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; connect-src 'self'; form-action 'self'; frame-ancestors 'none'; base-uri 'self'");
   res.setHeader("Permissions-Policy", "geolocation=(), microphone=(), camera=(), fullscreen=()");
   res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, private");
   res.setHeader("Pragma", "no-cache");
